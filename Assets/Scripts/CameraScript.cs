c597e5dfac30f5ea616113c33fdaef5a3eb40112
Adneycm <adneycm@al.insper.edu.br>
2024-04-27 16:38:31 -0300

    boss fight background and camera displacement

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     [SerializeField] private Transform playerTransform;
+    [SerializeField] private int upCamera;
     private void Update()
     {
-        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y+ upCamera, transform.position.z);
     }
 }
