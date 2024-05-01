28ac34760bce869e9e818f9e264cf08c89c840e0
Adneycm <adneycm@al.insper.edu.br>
2024-04-30 21:12:23 -0300

    update UI to appear collected potion

--- Assets/Scripts/PotionScript.cs
+++ Assets/Scripts/PotionScript.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PotionScript : MonoBehaviour
 {
+    [SerializeField] private Image potion;
     [SerializeField] private Sprite[] sprites;
+    private int potionIndex;
 
     void Start()
     {
@@ -15,6 +18,14 @@ public class PotionScript : MonoBehaviour
             // Select a random sprite from the array
             int randomIndex = Random.Range(0, sprites.Length);
             spriteRenderer.sprite = sprites[randomIndex];
+
+            potionIndex = randomIndex;
         }
     }
+
+    public void UpdatePotionCanvas()
+    {
+        potion.sprite = sprites[potionIndex];
+    }
+    
 }
